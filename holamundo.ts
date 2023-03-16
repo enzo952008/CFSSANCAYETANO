1b45fc78632c80e83ed812b00c362b46e1476bf4
enzo952008 <enzo9520@hotmail.com>
2023-03-16 13:01:49 -0300

    cambio 2

---
+++ holamundo.ts
@@ -0,0 +1,2 @@
+console.log ("hola mundo, vamos a cocinar un asado \n paso-1 salamos la carne \n paso-2 ponemos la carne \n paso-3 esperamos tiempo de coccion \n paso-4 retiramos la carne \n paso-5 a comer")
+ 
